130e83da7fed798566ea574781a1b1afcc8a1d8e
alterodoo <mauricio.guevara@alterben.com>
2026-02-03 12:34:36 -0500

    Delete alterben_mrp_master_order directory

--- alterben_mrp_master_order/models/res_company.py
+++
@@ -1,8 +0,0 @@
-from odoo import api, fields, models
-from odoo.exceptions import ValidationError
-
-
-class ResCompany(models.Model):
-    _inherit = "res.company"
-
-
--- alterben_mrp_master_order/models/res_config_settings.py
+++
@@ -1,7 +0,0 @@
-from odoo import api, fields, models
-from odoo.exceptions import ValidationError
-
-
-class ResConfigSettings(models.TransientModel):
-    _inherit = "res.config.settings"
-
